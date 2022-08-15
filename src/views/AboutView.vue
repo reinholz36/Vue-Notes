652a835caa2326c04cfeb65799c596d5506e3ce9
reinholz36 <dreinholz28@gmail.com>
2022-08-15 09:52:18 -0400

    added padding round text in about section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>About Vue Notes</h1>
 	<p>
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi, error dicta sunt velit doloribus voluptate autem odio magni est, fuga soluta voluptatum, aut itaque molestias delectus quisquam facere illum.
