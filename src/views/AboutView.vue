cde044072f344c9510297504ade20c3eef874626
reinholz36 <dreinholz28@gmail.com>
2022-08-15 09:32:01 -0400

    updated about section with title and lorem ipsum paragraphs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Vue Notes</h1>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi, error dicta sunt velit doloribus voluptate autem odio magni est, fuga soluta voluptatum, aut itaque molestias delectus quisquam facere illum.
+	</p>
+		<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi, error dicta sunt velit doloribus voluptate autem odio magni est, fuga soluta voluptatum, aut itaque molestias delectus quisquam facere illum.
+	</p>
+		<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi, error dicta sunt velit doloribus voluptate autem odio magni est, fuga soluta voluptatum, aut itaque molestias delectus quisquam facere illum.
+	</p>
   </div>
 </template>
