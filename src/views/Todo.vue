3376203ebb938fdfff8a5e76890d01075c8ce075
reinholz36 <dreinholz28@gmail.com>
2022-08-15 10:50:20 -0400

    refactored Todo file to remove helloworld template, also removed helloworld component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,14 @@
 <template>
-  <hello-world />
+  <div class="home pa-6">
+	<h1>
+		Todo Page
+	</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Todo',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
