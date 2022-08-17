804a442164104631913fbd1638ed155a82d27505
reinholz36 <dreinholz28@gmail.com>
2022-08-17 09:24:01 -0400

    removed v list item sub list component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <v-list
-      two-line
       flat
     >
 
@@ -16,55 +15,6 @@
 
 			<v-list-item-content>
 				<v-list-item-title>Notifications</v-list-item-title>
-				<v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
-			</v-list-item-content>
-			</template>
-		</v-list-item>
-
-		<v-list-item>
-			<template v-slot:default="{ active }">
-			<v-list-item-action>
-				<v-checkbox
-				:input-value="active"
-				color="primary"
-				></v-checkbox>
-			</v-list-item-action>
-
-			<v-list-item-content>
-				<v-list-item-title>Sound</v-list-item-title>
-				<v-list-item-subtitle>Hangouts message</v-list-item-subtitle>
-			</v-list-item-content>
-			</template>
-		</v-list-item>
-
-		<v-list-item>
-			<template v-slot:default="{ active }">
-			<v-list-item-action>
-				<v-checkbox
-				:input-value="active"
-				color="primary"
-				></v-checkbox>
-			</v-list-item-action>
-
-			<v-list-item-content>
-				<v-list-item-title>Video sounds</v-list-item-title>
-				<v-list-item-subtitle>Hangouts video call</v-list-item-subtitle>
-			</v-list-item-content>
-			</template>
-		</v-list-item>
-
-		<v-list-item>
-			<template v-slot:default="{ active }">
-			<v-list-item-action>
-				<v-checkbox
-				:input-value="active"
-				color="primary"
-				></v-checkbox>
-			</v-list-item-action>
-
-			<v-list-item-content>
-				<v-list-item-title>Invites</v-list-item-title>
-				<v-list-item-subtitle>Notify when receiving invites</v-list-item-subtitle>
 			</v-list-item-content>
 			</template>
 		</v-list-item>
