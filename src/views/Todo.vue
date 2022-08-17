4f77400e4dd383cc39890fd779c8dafc5c07ace4
reinholz36 <dreinholz28@gmail.com>
2022-08-17 10:00:57 -0400

    corrected top padding space

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,53 +1,54 @@
 <template>
-  <div class="home">
-    <v-list
-      flat
-    >
-	<div
-		v-for="task in tasks"
-		:key="task.id">
-		<v-list-item>
-			<template v-slot:default="{ active, }">
-			<v-list-item-action>
-				<v-checkbox
-				:input-value="active"
-				color="primary"
-				></v-checkbox>
-			</v-list-item-action>
+	<div class="home">
+		<v-list
+			class="pt-0"
+			flat
+		>
+		<div
+			v-for="task in tasks"
+			:key="task.id">
+			<v-list-item>
+				<template v-slot:default="{ active, }">
+				<v-list-item-action>
+					<v-checkbox
+					:input-value="active"
+					color="primary"
+					></v-checkbox>
+				</v-list-item-action>
 
-			<v-list-item-content>
-				<v-list-item-title> {{ task.title }} </v-list-item-title>
-			</v-list-item-content>
-			</template>
+				<v-list-item-content>
+					<v-list-item-title> {{ task.title }} </v-list-item-title>
+				</v-list-item-content>
+				</template>
 
-		</v-list-item>
-		<v-divider></v-divider>
+			</v-list-item>
+			<v-divider></v-divider>
+		</div>
+		</v-list>
 	</div>
-    </v-list>
-  </div>
 </template>
 
 <script>
 
-  export default {
-    name: 'Todo',
-	data() {
-		return {
-			tasks: [
-				{
-					id: 1,
-					title: 'Coffee ☕️'
-				},
-				{
-					id: 2,
-					title: 'Attend to Slack messages'
-				},
-				{
-					id: 3,
-					title: 'Attend to All not-yet-merged PRs'
-				},
-			]
+	export default {
+		name: 'Todo',
+		data() {
+			return {
+				tasks: [
+					{
+						id: 1,
+						title: 'Coffee ☕️'
+					},
+					{
+						id: 2,
+						title: 'Attend to Slack messages'
+					},
+					{
+						id: 3,
+						title: 'Attend to All not-yet-merged PRs'
+					},
+				]
+			}
 		}
 	}
-  }
 </script>
