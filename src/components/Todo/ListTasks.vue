49d32c8b0ec2c91a6e542f3f5bae464f90e20439
reinholz36 <dreinholz28@gmail.com>
2022-09-02 09:50:20 -0400

    create task list child component

---
+++ src/components/Todo/ListTasks.vue
@@ -0,0 +1,51 @@
+<template>
+	<v-list
+		class="pt-0"
+		flat
+	>
+	<div
+		v-for="task in $store.state.tasks"
+		:key="task.id">
+		<v-list-item
+		@click="$store.commit('doneTask', task.id)"
+		:class="{'green lighten-5' : task.done}"
+		>
+			<template v-slot:default>
+			<v-list-item-action>
+				<v-checkbox
+				:input-value="task.done"
+				color="primary"
+				></v-checkbox>
+			</v-list-item-action>
+
+			<v-list-item-content>
+				<v-list-item-title
+				:class="{'text-decoration-line-through' : task.done}"
+				> {{ task.title }} </v-list-item-title>
+			</v-list-item-content>
+
+			<v-list-item-action>
+				<v-btn
+				@click.stop="$store.commit('deleteTask', task.id)"
+
+				icon>
+					<v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
+				</v-btn>
+			</v-list-item-action>
+			</template>
+
+		</v-list-item>
+		<v-divider></v-divider>
+	</div>
+	</v-list>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,53 +1,16 @@
 <template>
 	<div class="home">
 		<field-add-task />
-			<v-list
+			<list-tasks
 				v-if="$store.state.tasks.length"
-				class="pt-0"
-				flat
-			>
-			<div
-				v-for="task in $store.state.tasks"
-				:key="task.id">
-				<v-list-item
-				@click="$store.commit('doneTask', task.id)"
-				:class="{'green lighten-5' : task.done}"
-				>
-					<template v-slot:default>
-					<v-list-item-action>
-						<v-checkbox
-						:input-value="task.done"
-						color="primary"
-						></v-checkbox>
-					</v-list-item-action>
-
-					<v-list-item-content>
-						<v-list-item-title
-						:class="{'text-decoration-line-through' : task.done}"
-						> {{ task.title }} </v-list-item-title>
-					</v-list-item-content>
-
-					<v-list-item-action>
-						<v-btn
-						@click.stop="$store.commit('deleteTask', task.id)"
-
-						icon>
-							<v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
-						</v-btn>
-					</v-list-item-action>
-					</template>
-
-				</v-list-item>
-				<v-divider></v-divider>
-			</div>
-			</v-list>
+			/>
 		<div
-		v-else
-		class="no-tasks"
+			v-else
+			class="no-tasks"
 		>
 		    <v-icon
-			size="6rem"
-			color="primary"
+				size="6rem"
+				color="primary"
 			>
 				mdi-check
 			</v-icon>
@@ -59,14 +22,18 @@
 </template>
 
 <script>
-		import FieldAddTask from "../components/Todo/FieldAddTask.vue"
-	export default {
-		components: {
-			'field-add-task': FieldAddTask
-		},
-		name: 'Todo',
-	}
+import ListTasksVue from '@/components/Todo/ListTasks.vue'
+import FieldAddTask from "../components/Todo/FieldAddTask.vue"
 
+export default {
+
+		name: 'Todo',
+
+		components: {
+			'field-add-task': FieldAddTask,
+			'list-tasks': ListTasksVue
+		},
+	}
 </script>
 
 <style lang="sass">
