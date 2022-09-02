245f4e357319701a93ab8c5e6369f11700a496df
reinholz36 <dreinholz28@gmail.com>
2022-09-02 13:32:12 -0400

    created task child component

--- src/components/Todo/ListTasks.vue
+++ src/components/Todo/ListTasks.vue
@@ -3,46 +3,22 @@
 		class="pt-0"
 		flat
 	>
-	<div
+
+	<tasks
 		v-for="task in $store.state.tasks"
-		:key="task.id">
-		<v-list-item
-		@click="$store.commit('doneTask', task.id)"
-		:class="{'green lighten-5' : task.done}"
-		>
-			<template v-slot:default>
-			<v-list-item-action>
-				<v-checkbox
-				:input-value="task.done"
-				color="primary"
-				></v-checkbox>
-			</v-list-item-action>
+		:key="task.id"
+		:task="task"
+	/>
 
-			<v-list-item-content>
-				<v-list-item-title
-				:class="{'text-decoration-line-through' : task.done}"
-				> {{ task.title }} </v-list-item-title>
-			</v-list-item-content>
-
-			<v-list-item-action>
-				<v-btn
-				@click.stop="$store.commit('deleteTask', task.id)"
-
-				icon>
-					<v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
-				</v-btn>
-			</v-list-item-action>
-			</template>
-
-		</v-list-item>
-		<v-divider></v-divider>
-	</div>
 	</v-list>
 </template>
 
 <script>
+	import Tasks from "@/components/Todo/Task.vue"
 export default {
-
+	components: {
+		'tasks': Tasks,
+	}
 }
 </script>
 
---
+++ src/components/Todo/Task.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+	<v-list-item
+	@click="$store.commit('doneTask', task.id)"
+	:class="{'green lighten-5' : task.done}"
+	>
+		<template v-slot:default>
+		<v-list-item-action>
+			<v-checkbox
+			:input-value="task.done"
+			color="primary"
+			></v-checkbox>
+		</v-list-item-action>
+
+		<v-list-item-content>
+			<v-list-item-title
+			:class="{'text-decoration-line-through' : task.done}"
+			> {{ task.title }} </v-list-item-title>
+		</v-list-item-content>
+
+		<v-list-item-action>
+			<v-btn
+			@click.stop="$store.commit('deleteTask', task.id)"
+
+			icon>
+				<v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
+			</v-btn>
+		</v-list-item-action>
+		</template>
+
+	</v-list-item>
+	<v-divider></v-divider>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['task']
+}
+</script>
+
+<style>
+
+</style>
