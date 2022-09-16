23adc266ad87ec89e1aae3b777ece58fa5ebd089
reinholz36 <dreinholz28@gmail.com>
2022-09-16 12:48:40 -0400

    tied delete methods to menu selector

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -1,52 +1,40 @@
 <template>
-  <div>
-	<v-list-item
-	@click="$store.commit('doneTask', task.id)"
-	:class="{'green lighten-5' : task.done}"
-	>
-		<template v-slot:default>
-		<v-list-item-action>
-			<v-checkbox
-			:input-value="task.done"
-			color="primary"
-			></v-checkbox>
-		</v-list-item-action>
+    <div>
+		<v-list-item
+		@click="$store.commit('doneTask', task.id)"
+		:class="{'green lighten-5' : task.done}"
+		>
+			<template v-slot:default>
+				<v-list-item-action>
+					<v-checkbox
+					:input-value="task.done"
+					color="primary"
+					></v-checkbox>
+				</v-list-item-action>
 
-		<v-list-item-content>
-			<v-list-item-title
-			:class="{'text-decoration-line-through' : task.done}"
-			> {{ task.title }} </v-list-item-title>
-		</v-list-item-content>
+				<v-list-item-content>
+					<v-list-item-title
+					:class="{'text-decoration-line-through' : task.done}"
+					> {{ task.title }} </v-list-item-title>
+				</v-list-item-content>
 
-		<v-list-item-action>
-			<task-menu />
-		</v-list-item-action>
-		</template>
+				<v-list-item-action>
+					<task-menu
+					:task = 'task'
+					/>
+				</v-list-item-action>
+			</template>
 
-	</v-list-item>
-	<v-divider></v-divider>
-	<dialog-delete
-	v-if="dialogs.delete"
-	@no-selected-in-delete-dialog="dialogs.delete = false"
-	:task="task"
-	/>
+		</v-list-item>
+		<v-divider></v-divider>
 	</div>
 </template>
 
 <script>
-import DialogDeleteVue from '@/components/Todo/Dialogs/DialogDelete.vue'
 import TaskMenuVue from '@/components/Todo/TaskMenu.vue'
 export default {
 	props: ['task'],
-	data() {
-		return {
-			dialogs: {
-				delete: false
-			}
-		}
-	},
 	components: {
-		'dialog-delete': DialogDeleteVue,
 		'task-menu': TaskMenuVue
 	}
 }
--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -1,39 +1,53 @@
 <template>
-	<v-menu
+	<div>
+		<v-menu
 		bottom
 		left
-	>
-		<template v-slot:activator="{ on, attrs }">
-			<v-btn
-				color="primary"
-				icon
-				v-bind="attrs"
-				v-on="on"
-			>
-				<v-icon>mdi-dots-vertical</v-icon>
-			</v-btn>
-		</template>
-
-		<v-list
-		rounded
 		>
-			<v-list-item
-				v-for="(item, index) in items"
-				:key="index"
-				@click="handleMenuSelect(index)"
+			<template v-slot:activator="{ on, attrs }">
+				<v-btn
+					color="primary"
+					icon
+					v-bind="attrs"
+					v-on="on"
+				>
+					<v-icon>mdi-dots-vertical</v-icon>
+				</v-btn>
+			</template>
+
+			<v-list
+			rounded
 			>
-				<v-list-item-icon>
-					<v-icon v-text="item.icon"></v-icon>
-				</v-list-item-icon>
-				<v-list-item-title>{{ item.title }}</v-list-item-title>
-			</v-list-item>
-		</v-list>
-	</v-menu>
+				<v-list-item
+					v-for="(item, index) in items"
+					:key="index"
+					@click="handleMenuSelect(index)"
+				>
+					<v-list-item-icon>
+						<v-icon v-text="item.icon"></v-icon>
+					</v-list-item-icon>
+					<v-list-item-title>{{ item.title }}</v-list-item-title>
+				</v-list-item>
+			</v-list>
+		</v-menu>
+
+		<dialog-delete
+	v-if="dialogs.delete"
+	@no-selected-in-delete-dialog="dialogs.delete = false"
+	:task="task"
+	/>
+	</div>
 </template>
 
 <script>
+import DialogDeleteVue from '@/components/Todo/Dialogs/DialogDelete.vue'
 export default {
+	props: ['task'],
     data: () => ({
+
+		dialogs: {
+			delete: false
+		},
 		items: [
 			{
 				title: 'Edit',
@@ -53,15 +67,18 @@ export default {
 				title: 'Delete',
 				icon: 'mdi-delete',
 				click() {
-					console.log('Delete clicked!')
+					this.dialogs.delete = true;
 				},
 			},
 		],
     }),
 	methods: {
 		handleMenuSelect(index){
-			this.items[index].click();
+			this.items[index].click.call(this);
 		}
+	},
+	components: {
+		'dialog-delete': DialogDeleteVue,
 	},
 }
 </script>
