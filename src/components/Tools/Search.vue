49c0a7bfa395dc406c7c5d2ceff27c2ed56c31dc
reinholz36 <dreinholz28@gmail.com>
2023-01-30 17:00:59 -0500

    added closing animation to search field

--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -1,6 +1,9 @@
 <template>
     <v-text-field
+	@focus="searchClosed = false"
+	@blur="searchClosed = true"
 		class="expanding-search mt-1"
+		:class="{'closed' : searchClosed }"
         placeholder="Search"
 		prepend-inner-icon="mdi-magnify"
         filled
@@ -11,14 +14,23 @@
 
 <script>
 export default {
-
+	data() {
+		return {
+			searchClosed: true
+		}
+	}
 }
 </script>
 
 <style lang="sass">
 	.expanding-search
+		transition: max-width 0.3s
 		.v-input__slot
 			&::before, &::after
 				border-color: transparent !important
+		&.closed
+			max-width: 45px
+			.v-input__slot
+				background: transparent !important
 
 </style>
