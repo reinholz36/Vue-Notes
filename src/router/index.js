e6789d4dd85edd00996a1d2fa6d23191ba2c7e1f
reinholz36 <dreinholz28@gmail.com>
2023-02-22 17:03:53 -0500

    added page name to title on route change

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -22,6 +22,11 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+	document.title = `Vue Notes - ${to.name}`
+	next()
 })
 
 export default router
